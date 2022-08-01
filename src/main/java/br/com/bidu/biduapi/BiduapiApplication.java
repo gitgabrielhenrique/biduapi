5c496c4ede7946c3ffc4a97a054ee89ae6b68319
Guilherme Martos <guilhermemartos2005@gmail.com>
2022-08-01 08:54:46 -0300

    Initial commit

---
+++ src/main/java/br/com/bidu/biduapi/BiduapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.bidu.biduapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiduapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiduapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bidu/biduapi/BiduapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bidu.biduapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiduapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

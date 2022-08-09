84b906b0509ac6b95f536a0046282e6e069776e0
Guilherme Martos <guilhermemartos2005@gmail.com>
2022-08-09 09:28:31 -0300

    Colocando os atributos e os 'Getters' e 'Setters'

--- src/main/java/br/com/bidu/biduapi/model/Curso.java
+++ src/main/java/br/com/bidu/biduapi/model/Curso.java
@@ -1,4 +1,23 @@
 package br.com.bidu.biduapi.model;
 
 public class Curso {
+
+    private Long id;
+    private String nomecurso;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomecurso() {
+        return nomecurso;
+    }
+
+    public void setNomecurso(String nomecurso) {
+        this.nomecurso = nomecurso;
+    }
 }

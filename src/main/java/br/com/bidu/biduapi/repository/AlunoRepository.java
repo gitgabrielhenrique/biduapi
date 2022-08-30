749152d93182ce6668187335c890326c3aa5559e
Gabriel Henrique dos Santos <gabrielHsantos2018@gmail.com>
2022-08-30 09:50:34 -0300

    criaçao dos repositorios

---
+++ src/main/java/br/com/bidu/biduapi/repository/AlunoRepository.java
@@ -0,0 +1,10 @@
+package br.com.bidu.biduapi.repository;
+
+import br.com.bidu.biduapi.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno,Long > {
+
+}

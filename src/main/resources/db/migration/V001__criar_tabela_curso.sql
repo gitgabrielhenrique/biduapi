ad4e9ed57c58431b15f045737205b8e81de496e8
Gabriel Henrique dos Santos <gabrielHsantos2018@gmail.com>
2022-09-12 08:54:27 -0300

    configurando tabela

---
+++ src/main/resources/db/migration/V001__criar_tabela_curso.sql
@@ -0,0 +1,13 @@
+Create table Curso(
+id BigInt not null primary key auto_increment,
+nomeCurso varchar(100)
+
+
+);
+
+insert into Curso(nomecurso) values ('Desenvolvimento de sistemas ');
+insert into Curso(nomecurso) values ('quimica');
+insert into Curso(nomecurso) values ('administração');
+insert into Curso(nomecurso) values ('logistica');
+insert into Curso(nomecurso) values ('marketing');
+insert into Curso(nomecurso) values ('edificaçoes');

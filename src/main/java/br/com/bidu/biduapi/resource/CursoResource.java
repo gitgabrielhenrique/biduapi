67a29eb7a9ac1a0bb781cf8acdd11f4e2eae8507
Gabriel Henrique dos Santos <gabrielHsantos2018@gmail.com>
2022-11-01 09:46:02 -0300

    Update CursoResource.java

--- src/main/java/br/com/bidu/biduapi/resource/CursoResource.java
+++ src/main/java/br/com/bidu/biduapi/resource/CursoResource.java
@@ -1,7 +1,23 @@
 package br.com.bidu.biduapi.resource;
 
+import br.com.bidu.biduapi.model.Curso;
+import br.com.bidu.biduapi.repository.CursoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("Cursos")
 public class CursoResource {
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+public List<Curso> listarTodosCursos(){
+    return
+}
+
+
 }
